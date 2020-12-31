82030219c6bbbf908a03c8d173ae8a294059a3c6
Michael Kuc <michaelkuc6@gmail.com>
2020-12-31 22:20:53 +0000

    Add clang-format formatting rules.
    Reformatted all code to new formatting standard.

--- src/Fast_klt.cpp
+++ src/Fast_klt.cpp
@@ -1,6 +1,7 @@
 #include "Fast_klt.h"
 
-void myKLT(const cv::Mat source, const cv::Mat target, vector<cv::Point2f> &sourceFeatures, vector<cv::Point2f> &targetFeatures){
+void myKLT(const cv::Mat source, const cv::Mat target, vector<cv::Point2f>& sourceFeatures,
+           vector<cv::Point2f>& targetFeatures) {
 
 	cv::Mat img0Gray = cv::Mat::zeros(source.rows, source.cols, CV_8UC1);
 	cv::Mat curImgGray = cv::Mat::zeros(target.rows, target.cols, CV_8UC1);
@@ -10,18 +11,21 @@ void myKLT(const cv::Mat source, const cv::Mat target, vector<cv::Point2f> &sour
 	vector<cv::Point2f> featurePtSet0;
 	int maxNum = 10000;
 	goodFeaturesToTrack(img0Gray, featurePtSet0, maxNum, 0.05, 5);
-	cornerSubPix(img0Gray, featurePtSet0, cv::Size(15, 15), cv::Size(-1, -1), cv::TermCriteria(cv::TermCriteria::MAX_ITER | cv::TermCriteria::EPS, 20, 0.03));
+	cornerSubPix(img0Gray, featurePtSet0, cv::Size(15, 15), cv::Size(-1, -1),
+	             cv::TermCriteria(cv::TermCriteria::MAX_ITER | cv::TermCriteria::EPS, 20, 0.03));
 
 	vector<cv::Point2f> curfeaturePtSet;
 	vector<uchar> curFlag;
 	vector<float> curErrSet;
-	calcOpticalFlowPyrLK(img0Gray, curImgGray, featurePtSet0, curfeaturePtSet, curFlag, curErrSet, cv::Size(15, 15));
+	calcOpticalFlowPyrLK(img0Gray, curImgGray, featurePtSet0, curfeaturePtSet, curFlag, curErrSet,
+	                     cv::Size(15, 15));
 	for (int p = 0; p < curErrSet.size(); p++)
-		if (curErrSet[p] > 100 || curfeaturePtSet[p].x < 0 || curfeaturePtSet[p].y < 0 || curfeaturePtSet[p].x > img0Gray.cols || curfeaturePtSet[p].y > img0Gray.rows)
+		if (curErrSet[p] > 100 || curfeaturePtSet[p].x < 0 || curfeaturePtSet[p].y < 0 ||
+		    curfeaturePtSet[p].x > img0Gray.cols || curfeaturePtSet[p].y > img0Gray.rows)
 			curFlag[p] = 0;
 
-	for (int i = 0; i<curFlag.size(); i++){
-		if (curFlag.at(i) == 1){
+	for (int i = 0; i < curFlag.size(); i++) {
+		if (curFlag.at(i) == 1) {
 			sourceFeatures.push_back(featurePtSet0.at(i));
 			targetFeatures.push_back(curfeaturePtSet.at(i));
 		}
--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,78 @@
+#include "Fast_klt.h"
+#include "MeshFlow.h"
+#include "VideoIO.h"
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include "VideoIO.h"
-#include "MeshFlow.h"
-#include "Fast_klt.h"
 
 using namespace cv;
 using namespace std;
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
 
-    char *name = "../data/test.avi";
+	char* name = "../data/test.avi";
 
-    double m_fps;
+	double m_fps;
 
-    vector<cv::Mat> m_frames = VideoIO::GetFrames(name, m_fps);
+	vector<cv::Mat> m_frames = VideoIO::GetFrames(name, m_fps);
 
-    cv::Size m_size = m_frames[0].size();
-    int m_height = m_size.height;
-    int m_width = m_size.width;
-    int m_frameNum = m_frames.size();
+	cv::Size m_size = m_frames[0].size();
+	int m_height = m_size.height;
+	int m_width = m_size.width;
+	int m_frameNum = m_frames.size();
 
-    m_frameNum = 40;
+	m_frameNum = 40;
 
-    vector<cv::Mat> map_X, map_Y;
+	vector<cv::Mat> map_X, map_Y;
 
-    map_X.resize(m_frameNum - 1);
-    for (int i = 0; i < map_X.size(); i++) map_X[i].create(m_size, CV_32F);
+	map_X.resize(m_frameNum - 1);
+	for (int i = 0; i < map_X.size(); i++)
+		map_X[i].create(m_size, CV_32F);
 
-    map_Y.resize(m_frameNum - 1);
-    for (int i = 0; i < map_Y.size(); i++) map_Y[i].create(m_size, CV_32F);
+	map_Y.resize(m_frameNum - 1);
+	for (int i = 0; i < map_Y.size(); i++)
+		map_Y[i].create(m_size, CV_32F);
 
-    MeshFlow meshflow(m_width, m_height);
-    vector<cv::Point2f> sourceFeatures, targetFeatures;
+	MeshFlow meshflow(m_width, m_height);
+	vector<cv::Point2f> sourceFeatures, targetFeatures;
 
-    cout << "MeshFlow starts." << endl;
-    for (int i = 1; i < m_frameNum; i++) {
-        meshflow.ReInitialize();
-        sourceFeatures.clear();
-        targetFeatures.clear();
+	cout << "MeshFlow starts." << endl;
+	for (int i = 1; i < m_frameNum; i++) {
+		meshflow.ReInitialize();
+		sourceFeatures.clear();
+		targetFeatures.clear();
 
-        myKLT(m_frames[i - 1], m_frames[i], sourceFeatures, targetFeatures);
-        meshflow.SetFeature(sourceFeatures, targetFeatures);
+		myKLT(m_frames[i - 1], m_frames[i], sourceFeatures, targetFeatures);
+		meshflow.SetFeature(sourceFeatures, targetFeatures);
 
-        meshflow.Execute();
-        meshflow.GetMotions(map_X[i-1], map_Y[i-1]);
-        cout << i << "...";
-    }
-    cout << "[DONE]" << endl;
+		meshflow.Execute();
+		meshflow.GetMotions(map_X[i - 1], map_Y[i - 1]);
+		cout << i << "...";
+	}
+	cout << "[DONE]" << endl;
 
-    /*
-     * Visualize
-     */
-    cv::namedWindow("MeshFlow");
-    for (int i = 1; i < m_frameNum; i++) {
+	/*
+	 * Visualize
+	 */
+	cv::namedWindow("MeshFlow");
+	for (int i = 1; i < m_frameNum; i++) {
 
-        cv::Mat canvas = m_frames[i-1].clone();
+		cv::Mat canvas = m_frames[i - 1].clone();
 
-        for (int meshVertex_X = 40; meshVertex_X < 640; meshVertex_X += 40){
-            for (int meshVertex_Y = 40; meshVertex_Y < 360; meshVertex_Y += 40){
-                cv::arrowedLine(canvas,
-                                cv::Point(meshVertex_X,
-                                          meshVertex_Y),
-                                cv::Point(meshVertex_X + map_X[i-1].at<float>(meshVertex_Y, meshVertex_X),
-                                          meshVertex_Y + map_Y[i-1].at<float>(meshVertex_Y, meshVertex_X)),
-                                cv::Scalar(255,0,0),2,8,0,0.6 );
-            }
-        }
+		for (int meshVertex_X = 40; meshVertex_X < 640; meshVertex_X += 40) {
+			for (int meshVertex_Y = 40; meshVertex_Y < 360; meshVertex_Y += 40) {
+				cv::arrowedLine(
+				    canvas, cv::Point(meshVertex_X, meshVertex_Y),
+				    cv::Point(meshVertex_X + map_X[i - 1].at<float>(meshVertex_Y, meshVertex_X),
+				              meshVertex_Y + map_Y[i - 1].at<float>(meshVertex_Y, meshVertex_X)),
+				    cv::Scalar(255, 0, 0), 2, 8, 0, 0.6);
+			}
+		}
 
-        cv::imshow("MeshFlow", canvas);
-        cv::waitKey(0);
-    }
+		cv::imshow("MeshFlow", canvas);
+		cv::waitKey(0);
+	}
 
-    cv::destroyAllWindows();
+	cv::destroyAllWindows();
 
 	return 0;
 }
